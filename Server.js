3dcaa9569e05a01886b8f33c0e51edb29a566337
venkatsammm <venkatkishoreraman@gmail.com>
2024-02-15 14:07:11 +0530

    ok

--- Server.js
+++ Server.js
@@ -3,6 +3,10 @@ var app=express()
 app.get("/ping",(req,res)=>{
     res.send("pong")
 })
+app.get("",(req,res)=>{
+    res.send("Hello guys")
+})
+
 
 app.listen(3000,()=>{
     console.log("hello")
