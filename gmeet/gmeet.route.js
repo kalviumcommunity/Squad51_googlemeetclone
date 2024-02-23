87030f959ec88e2375175a1b9ca494aadf498d25
venkatsammm <venkatkishoreraman@gmail.com>
2024-02-23 09:48:40 +0530

    ok

--- gmeet/gmeet.route.js
+++ gmeet/gmeet.route.js
@@ -6,6 +6,10 @@ const postRouter = express.Router();
 const putRouter = express.Router();
 const deleteRouter = express.Router();
 const UserDetails = require("../Model/userDetails.model");
+getRouter.use(express.json());
+postRouter.use(express.json());
+putRouter.use(express.json());
+deleteRouter.use(express.json());
 
 getRouter.get('/getallusers', async (req, res) => {
     try {
@@ -21,7 +25,7 @@ getRouter.get('/getallusers', async (req, res) => {
 
 getRouter.get('/getuser/:id', async (req, res) => {
     try {
-        const user = await UserDetails.findOne({ _id: req.params.id });
+        const user = await UserDetails.findOne({ id: req.params.id });
         res.status(200).json(user);
     } catch (err) {
         console.log(err);
@@ -33,8 +37,8 @@ getRouter.get('/getuser/:id', async (req, res) => {
 
 postRouter.post('/adduser', async (req, res) => {
     try {
-        const { name, lastname, email, id, Meeting_id, Title, Link } = req.body;
-        const user = await UserDetails.create({ name, lastname, email, id, Meeting_id, Title, Link });
+        const { name, lastname, email, id, Meetingid, Title, Link } = req.body;
+        const user = await UserDetails.create({ name, lastname, email, id, Meetingid, Title, Link });
         res.status(201).json(user);
     } catch (err) {
         console.log(err);
@@ -44,23 +48,37 @@ postRouter.post('/adduser', async (req, res) => {
     }
 });
 
-putRouter.put('/updateuser/:id', async (req, res) => {
+putRouter.patch('/updateuser/:id', async (req, res) => {
     try {
-        const { name, lastname, email, id, Meeting_id, Title, Link } = req.body;
-        const user = await UserDetails.findOneAndUpdate({ _id: req.params.id }, { name, lastname, email, id, Meeting_id, Title, Link }, { new: true });
+        const userId = req.params.id;
+        const updateFields = req.body;
+
+        if (!Object.keys(updateFields).length) {
+            return res.status(400).json({ message: "No fields provided for update" });
+        }
+
+        const user = await UserDetails.findOneAndUpdate(
+            {id:userId},
+            { $set: updateFields },
+            { new: true }
+        );
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
         res.status(200).json(user);
     } catch (err) {
         console.log(err);
-        return res.status(500).send({
-            message: "Internal server error"
-        });
+        return res.status(500).json({ message: "Internal server error" });
     }
 });
 
+
 deleteRouter.delete('/deleteuser/:id', async (req, res) => {
     try {
-        const user = await UserDetails.findOneAndDelete({ _id: req.params.id });
-        res.status(200).json(user);
+        const user = await UserDetails.findOneAndDelete({ id: req.params.id });
+        res.status(200).json("Deleted user");
     } catch (err) {
         console.log(err);
         return res.status(500).send({
